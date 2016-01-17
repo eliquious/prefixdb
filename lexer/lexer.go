22d491573ead93f1191bbcc2f2e5380da65ba097
eliquious <eliquious@gmail.com>
2016-01-16 22:07:52 -0600

    Add lexical tokens and cli for lexing

---
+++ lexer/lexer.go
@@ -0,0 +1,77 @@
+package lexer
+
+import (
+	"github.com/eliquious/lexer"
+)
+
+func init() {
+	// Loads keyword tokens into lexer
+	lexer.LoadTokenMap(tokenMap)
+}
+
+const (
+	// Starts the keywords with an offset from the built in tokens
+	startKeywords lexer.Token = iota + 1000
+
+	// CREATE starts a CREATE KEYSPACE query.
+	CREATE
+
+	// SELECT starts a SELECT FROM query.
+	SELECT
+
+	// DELETE deletes keys from a keyspace.
+	DELETE
+
+	// DROP deletes an entire keyspace.
+	DROP
+
+	// FROM specifies which keyspace to select and delete from.
+	FROM
+
+	// WHERE allows for key filtering when selecting or deleting.
+	WHERE
+
+	// KEYSPACE signifies what is being created.
+	KEYSPACE
+
+	// WITH is used to prefix query options.
+	WITH
+
+	// KEY signifies a key attribute follows.
+	KEY
+
+	// KEYS signifies several key attributes follow.
+	KEYS
+	endKeywords
+
+	// Separates the keywords from the conditionals
+	startConditionals
+
+	// BETWEEN filters an attribute by two values.
+	BETWEEN
+	endConditionals
+)
+
+var tokenMap = map[lexer.Token]string{
+
+	CREATE:   "CREATE",
+	SELECT:   "SELECT",
+	DELETE:   "DELETE",
+	DROP:     "DROP",
+	FROM:     "FROM",
+	WHERE:    "WHERE",
+	KEYSPACE: "KEYSPACE",
+	WITH:     "WITH",
+	KEY:      "KEY",
+	BETWEEN:  "BETWEEN",
+}
+
+// IsKeyword returns true if the token is a keyword.
+func IsKeyword(tok lexer.Token) bool {
+	return tok > startKeywords && tok < endKeywords
+}
+
+// IsConditional returns true if the token is a conditional clause.
+func IsConditional(tok lexer.Token) bool {
+	return tok > startConditionals && tok < endConditionals
+}
